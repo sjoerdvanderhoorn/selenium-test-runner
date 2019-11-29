4a03ffb92128230dc23582fbb573ed57b5751215
Sjoerd van der Hoorn <sjoerdvanderhoorn@hotmail.com>
2019-11-29 17:00:26 +0100

    Fix install

--- install.cmd
+++ install.cmd
@@ -2,7 +2,7 @@
 
 rem Install file assocations for .side
 assoc .side=sideFile
-ftype sideFile="C:\Program Files\nodejs\node.exe" "C:\Prive\selenium-test-runner\index.js" "%1"
+ftype sideFile="C:\Program Files\nodejs\node.exe" "C:\Prive\selenium-test-runner\index.js" "%%1"
 
 rem Install npm dependencies
 npm install
