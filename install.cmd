8776bc4244f643a6540ce377a8fd85ef060dbad9
Sjoerd van der Hoorn <sjoerdvanderhoorn@hotmail.com>
2019-12-04 16:30:22 +0100

    Add chromedriver to path during installation

--- install.cmd
+++ install.cmd
@@ -16,7 +16,10 @@ pause
 
 rem Install file assocations for .side
 assoc .side=sideFile
-ftype sideFile="C:\Program Files\nodejs\node.exe" "C:\Prive\selenium-test-runner\index.js" "%%1"
+ftype sideFile="C:\Program Files\nodejs\node.exe" "%~dp0\index.js" "%%1"
+
+rem Add chromedriver to path
+setx path "%path%;%appdata%\npm\node_modules\chromedriver\lib\chromedriver"
 
 rem Install npm dependencies
 npm install
